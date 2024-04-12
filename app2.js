4f861c27aa91d9412b6887f0b99ab8113d88430b
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-04-12 10:30:56 +0530

    new commit

--- app2.js
+++ app2.js
@@ -16,9 +16,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post('/api/admin-upload-pdf', upload.single('file'), async (req, res) => {
+app.post('/api/upload-pdf', upload.single('file'), async (req, res) => {
     const authToken = req.headers['x-access-token'];
-    if (!authToken) {
+    if (!authToken ) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
 
@@ -33,31 +33,11 @@ app.post('/api/admin-upload-pdf', upload.single('file'), async (req, res) => {
 
     // Start processing if not already doing so
     if (!isProcessing) {
-        processQueue('/admin/admin-upload-pdf');
-    }
-});
-app.post('/api/volunteer-upload-pdf', upload.single('file'), async (req, res) => {
-    const authToken = req.headers['x-access-token'];
-    if (!authToken) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-
-    // Add the task to the queue
-    queue.push({
-        buffer: req.file.buffer,
-        token: authToken,
-        metadata: req.body // This includes any additional data like booth, district, etc.
-    });
-
-    res.status(202).json({ message: "Upload received, queued for processing." });
-
-    // Start processing if not already doing so
-    if (!isProcessing) {
-        processQueue('/volunteer/volunteer-upload-pdf');
+        processQueue();
     }
 });
 
-async function processQueue(endpoint) {
+async function processQueue() {
     if (queue.length === 0) {
         isProcessing = false;
         return;
@@ -68,7 +48,7 @@ async function processQueue(endpoint) {
 
     try {
         const result = await pdftoJson(buffer);
-        await axios.post(`${process.env.SERVER_URL}/api/${endpoint}`, {
+        await axios.post(`${process.env.SERVER_URL}/api/target-endpoint`, {
             data: result,
             ...metadata
         }, {
@@ -83,9 +63,10 @@ async function processQueue(endpoint) {
         console.error('Error during processing or upload', error);
     } finally {
         // Optionally clear cache or temp data here
-        processQueue(endpoint); // Process the next item in the queue
+        processQueue(); // Process the next item in the queue
     }
 }
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port ' + (process.env.PORT || 3000));
 });
